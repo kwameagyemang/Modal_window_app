12213a20e4b7ad88b2d979115c68721858fcab15
Kwame Agyemang <agyemangyeb@gmailcom>
2022-03-04 17:29:10 -0500

    progress

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnOpenModal);
 
-// looping over all show-model class
-for (let i = 0; i < btnOpenModal.length; i++) {
-    btnOpenModal[i].addEventListener('click', function () {
-        console.log('Button clicked');
-        // this displays modal button when clicked by removing hidden class
+const openModal = function () {
+    // this displays modal button when clicked by removing hidden class
         // theres is no . before hidden because we are just selecting class not the selector
-        modal.classList.remove('hidden',)
-        overlay.classList.remove('hidden')
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 
-    })
-} 
+}
 
 const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 }
+
+// looping over all show-model class
+for (let i = 0; i < btnOpenModal.length; i++) {
+    btnOpenModal[i].addEventListener('click', openModal);
+} 
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
