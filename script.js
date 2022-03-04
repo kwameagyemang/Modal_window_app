dc43a9ae94fe8f57cd08ce86d6ccc2fd118e6cb7
Kwame Agyemang <agyemangyeb@gmailcom>
2022-03-04 17:24:03 -0500

    commit in progress

--- script.js
+++ script.js
@@ -18,7 +18,10 @@ for (let i = 0; i < btnOpenModal.length; i++) {
     })
 } 
 
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
-})
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
